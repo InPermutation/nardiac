eaba4c8e12ab94f97bfee5a535c52a5ed4957489
Jacob Krall <jacob@jkrall.net>
2019-06-13 13:22:50 -0400

    Cell: immutable, simpler constructor

--- Cell.cs
+++ Cell.cs
@@ -7,14 +7,13 @@ namespace NARDIAC
         public readonly byte Item1;
         public readonly byte Item2;
         public readonly byte Item3;
-        public bool Negative;
+        public readonly bool Negative;
 
-        public Cell(byte a, byte b, byte c) : this(a, b, c, false) { }
-        public Cell(byte a, byte b, byte c, bool negative)
+        public Cell(byte a, byte b, byte c, bool negative = false)
         {
-            if (a > 9) throw new ArgumentException("Out of bounds", "a");
-            if (b > 9) throw new ArgumentException("Out of bounds", "b");
-            if (c > 9) throw new ArgumentException("Out of bounds", "c");
+            if (a > 9) throw new ArgumentException("Out of bounds", nameof(a));
+            if (b > 9) throw new ArgumentException("Out of bounds", nameof(b));
+            if (c > 9) throw new ArgumentException("Out of bounds", nameof(c));
 
             Item1 = a; Item2 = b; Item3 = c; Negative = negative;
         }
