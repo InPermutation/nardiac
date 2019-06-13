7a02e2b741e6693779b524736ae730c3ba4c2572
Jacob Krall <jacob@jkrall.net>
2019-06-13 13:35:40 -0400

    Computer: define and use Opcode enum

--- Computer.cs
+++ Computer.cs
@@ -2,6 +2,20 @@
 
 namespace NARDIAC
 {
+    public enum Opcode : byte
+    {
+        INP = 0,
+        CLA = 1,
+        ADD = 2,
+        TAC = 3,
+        SFT = 4,
+        OUT = 5,
+        STO = 6,
+        SUB = 7,
+        JMP = 8,
+        HRS = 9,
+    }
+
     public class Computer
     {
         bool paused = true;
@@ -38,46 +52,46 @@ namespace NARDIAC
 
             int xy = ir.Item2 * 10 + ir.Item3;
             int @int;
-            switch (ir.Item1)
+            switch ((Opcode)ir.Item1)
             {
-                case 0: // 0XY INP - Read input card into cell XY
+                case Opcode.INP: // 0XY INP - Read input card into cell XY
                     memory[xy] = input.Read();
                     break;
-                case 1: // 1XY CLA - Clear accumulator and add into it the contents of cell XY
+                case Opcode.CLA: // 1XY CLA - Clear accumulator and add into it the contents of cell XY
                     a = memory[xy];
                     break;
-                case 2: // 2XY ADD - Add contents of cell XY into accumulator.
+                case Opcode.ADD: // 2XY ADD - Add contents of cell XY into accumulator.
                     @int = IntFromCell(a) + IntFromCell(memory[xy]);
                     a = CellFromInt(@int);
                     if (@int < 0) @int = -@int;
                     carry = (byte)(@int / 1000);
                     break;
-                case 3: // 3XY TAC - Test accumulator and jump if negative.
+                case Opcode.TAC: // 3XY TAC - Test accumulator and jump if negative.
                     if (a.Negative)
                     {
                         // do not save old pc to *99
                         pc = xy;
                     }
                     break;
-                case 4: // 4XY SFT - Shift accumulator X left, then Y right
+                case Opcode.SFT: // 4XY SFT - Shift accumulator X left, then Y right
                     throw new NotImplementedException("SFT");
-                case 5: // 5XY OUT - Print contents of cell XY on output card
+                case Opcode.OUT: // 5XY OUT - Print contents of cell XY on output card
                     output.Write(memory[xy]);
                     break;
-                case 6: // 6XY STO - Store contents of accumulator in cell XY.
+                case Opcode.STO: // 6XY STO - Store contents of accumulator in cell XY.
                     memory[xy] = a;
                     break;
-                case 7: // 2XY SUB - Subtract contents of cell XY from accumulator.
+                case Opcode.SUB: // 2XY SUB - Subtract contents of cell XY from accumulator.
                     @int = IntFromCell(a) - IntFromCell(memory[xy]);
                     a = CellFromInt(@int);
                     if (@int < 0) @int = -@int;
                     carry = (byte)(@int / 1000);
                     break;
-                case 8: // 8XY JMP - Jump to XY and save PC to *99
+                case Opcode.JMP: // 8XY JMP - Jump to XY and save PC to *99
                     memory[99] = CellFromInt((pc % 100) + 800);
                     pc = xy;
                     break;
-                case 9: // 9XY HRS - Halt and reset program counter to XY. (usually 900)
+                case Opcode.HRS: // 9XY HRS - Halt and reset program counter to XY. (usually 900)
                     pc = xy;
                     this.paused = true;
                     break;
